885b0eb809c72800aa9f7afda3f5018695f9a654
James Nuttall <james@ghostwhite.net>
2020-01-16 15:54:27 +0000

    Remove peerDependancy

--- docs/postcss.config.js
+++ docs/postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   plugins: [
     require("postcss-import"),
-    require("tailwindcss")("./tailwind.config.js"),
+    require("@hitmarker/bricks/node_modules/tailwindcss")("./tailwind.config.js"),
     require("autoprefixer")
   ]
 };
