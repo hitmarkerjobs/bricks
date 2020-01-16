034f253948578afa54cf6f4e2c5714d5e1e0f450
Philip Huggan <phil@huggan.co.uk>
2020-01-16 16:40:34 +0000

    .

--- docs/postcss.config.js
+++ docs/postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   plugins: [
     require("postcss-import"),
-    require("@hitmarker/bricks/node_modules/tailwindcss")("./tailwind.config.js"),
+    require("tailwindcss")("./tailwind.config.js"),
     require("autoprefixer")
   ]
 };
