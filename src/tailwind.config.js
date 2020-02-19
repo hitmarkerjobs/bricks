1b9a87fdc3418dfe16b784d2054e4817735e7599
Philip Huggan <phil@Philips-MacBook-Pro.local>
2020-02-19 18:21:33 +0000

    z index

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -21,6 +21,20 @@ module.exports = {
       outline: '0 0 0 1px rgb(var(--color-brand))',
       none: 'none',
     },
+    zIndex: {
+      auto: 'auto',
+      '0': '0',
+      '1': '1',
+      '2': '2',
+      '3': '3',
+      '4': '4',
+      '5': '5',
+      '6': '6',
+      '7': '7',
+      '8': '8',
+      '9': '9',
+      '10': '10',
+    },
     customForms: theme => ({
       default: {
         'input, textarea, select': {
--- src/theme.config.js
+++ src/theme.config.js
@@ -12,6 +12,7 @@ const mainTheme = new Theme()
     'foxtrot': '#14171A',
 
     'brand': '#FF5800',
+    'black': '#000000',
     'white': '#FFFFFF',
     'green': '#17BF63',
     'yellow': '#FFAD1F',
