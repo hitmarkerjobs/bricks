72153027540774d417c3890db7d735262fa11a37
Philip Huggan <phil@philips-mbp.lan>
2020-03-04 00:46:29 +0000

    bin off custom z index

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -21,20 +21,6 @@ module.exports = {
       outline: '0 0 0 1px rgb(var(--color-brand))',
       none: 'none',
     },
-    zIndex: {
-      auto: 'auto',
-      '0': '0',
-      '1': '1',
-      '2': '2',
-      '3': '3',
-      '4': '4',
-      '5': '5',
-      '6': '6',
-      '7': '7',
-      '8': '8',
-      '9': '9',
-      '10': '10',
-    },
     fill: theme => ({
       'current': 'currentColor',
 
